ef4ca920ae9670cc103a4ddf3fcdad10df5c1e33
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-05-21 11:13:44 +0200

    [#11] v1.16.3

--- babel.module.config.js
+++ babel.module.config.js
@@ -1,4 +1,9 @@
 export default {
+  plugins: [
+    // TODO: This adds explicit extensions to imports inside generated ES modules,
+    // without which some tools currently fail to lookup relative imports.
+    'babel-plugin-add-import-extension',
+  ],
   presets: [
     ['./config/babel/preset', {
       modules: false,
