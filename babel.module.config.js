ae4d3f16e9e71345f5d16ad769ac16276d362096
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-06-10 10:38:27 +0200

    [#13] Use MJS extension for module build

--- babel.module.config.js
+++ babel.module.config.js
@@ -3,6 +3,9 @@ export default {
     // TODO: This adds explicit extensions to imports inside generated ES modules,
     // without which some tools currently fail to lookup relative imports.
     'babel-plugin-add-import-extension',
+    ['babel-plugin-replace-import-extension', {
+      extMapping: { '.js': '.mjs' },
+    }],
   ],
   presets: [
     ['./config/babel/preset', {
